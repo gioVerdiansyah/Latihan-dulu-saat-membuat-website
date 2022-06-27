b919cd805521e8f5df584bf7a86ff467c2386361
gioverdiansyah <gioverdiansyh@gmail.com>
2022-06-27 21:09:13 +0700

    first commit

---
+++ belajar-kondisi.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+        <title>jual-barang</title>
+        
+    </head>
+    <body>
+        <script>
+        var hargabarang= 10000
+        var jumlahuang= 50000
+        
+        if(hargabarang == jumlahuang){
+            document.writeln('p')
+           [0].innerHTML='kamu dapat membelinya,uang kamu pas'
+        }
+        </script>
+    </body>
+</html>
---
+++ popup.js
@@ -0,0 +1,16 @@
+    <script type="text/javascript" charset="utf-8">
+        //aku & kamu
+alert ("xixixixi>////<")
+        const nama = prompt("masukin nama dulu atuh neng")
+        
+        alert("sebenernya aku mau ngomong " + nama)
+       
+        const iyaAtauTidak = confirm("apakah kamu suka aku " + nama + "?")
+        
+        if (iyaAtauTidak) {
+            alert ("i love you to " + nama)
+        }
+        else {
+            alert ("ku menangisssss")
+        }
+    </script>
